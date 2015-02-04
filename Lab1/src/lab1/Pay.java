fb2f00618fea9e6b7ff46d002be02ba073f3a8f3
class <class@CST_N417_23.cst.citytech.cuny.edu>
2015-02-04 17:00:51 -0500

    replace the author comment

--- Lab1/src/lab1/Pay.java
+++ Lab1/src/lab1/Pay.java
@@ -5,8 +5,9 @@ import java.util.Scanner;  	//to be able to read from the keyboard
 /**
  * This program calculates the user's gross pay.
  *
- * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi
- * Khatchadourian</a>
+ * @author <a href="mailto:mohammed.alqaifi@mail.citytech.cuny.edu">mohammed
+ * alqaifi
+ * </a>
  */
 public class Pay {
 
