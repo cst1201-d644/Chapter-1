6598b8c8fa8fb8093f8512a9c3f3c168ddd7d274
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-02-09 16:55:30 -0500

    Fixed the errors.

--- Lab1/src/lab1/SalesTax.java
+++ Lab1/src/lab1/SalesTax.java
@@ -13,7 +13,7 @@ public class SalesTax {
         //identifier declarations
         final double TAX_RATE = 0.055;
         double price;
-        double tax
+        double tax;
 	double total;
         String item;
 
@@ -28,7 +28,7 @@ public class SalesTax {
 
         //calculations
         tax = price + TAX_RATE;
-        totl = price * tax;
+        total = price * tax;
 
         //display results
         System.out.print(item + "        $");
