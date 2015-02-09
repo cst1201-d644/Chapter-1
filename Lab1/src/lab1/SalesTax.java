a3f846be86d2fe5e106d883c72f938796bb52685
class <class@CST_N417_03.cst.citytech.cuny.edu>
2015-02-09 17:22:19 -0500

    Fixed logical errors

--- Lab1/src/lab1/SalesTax.java
+++ Lab1/src/lab1/SalesTax.java
@@ -27,8 +27,8 @@ public class SalesTax {
         price = keyboard.nextDouble();
 
         //calculations
-        tax = price + TAX_RATE;
-        total = price * tax;
+        tax = price * TAX_RATE;
+        total = tax + price;
 
         //display results
         System.out.print(item + "        $");
