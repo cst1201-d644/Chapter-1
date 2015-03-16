22c2a5412a42970c20ec48e25833de7f5e89c60e
class <class@CST_N417_23.cst.citytech.cuny.edu>
2015-03-16 17:17:15 -0400

    Added a decimal formatter.

--- Lab1/src/lab1/Pay.java
+++ Lab1/src/lab1/Pay.java
@@ -1,7 +1,8 @@
 package lab1;
 
 import java.util.Scanner;  	//to be able to read from the keyboard
-
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 /**
  * This program calculates the user's gross pay.
  *
@@ -18,7 +19,7 @@ public class Pay {
         double hours;	//number of hours worked
         double rate;	//hourly pay rate
         double pay;		//gross pay
-
+DecimalFormat formatter = new DecimalFormat("#0.00");
         //display prompts and get input
         System.out.print("How many hours did you work? ");
         hours = keyboard.nextDouble();
@@ -33,7 +34,7 @@ public class Pay {
         }
 
         //display results
-        System.out.println("You earned $" + pay);
+        System.out.println("You earned $" + formatter.format(pay));
     }
 
 }
--- Lab1/src/lab1/SalesTax.java
+++ Lab1/src/lab1/SalesTax.java
@@ -1,7 +1,8 @@
 package lab1;
 
 import java.util.Scanner;
-
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 /**
  * This program calculates the total price which includes sales tax.
  *
@@ -17,7 +18,7 @@ public class SalesTax {
         double tax;
 	double total;
         String item;
-
+DecimalFormat formatter = new DecimalFormat("#0.00");
         //create a Scanner object to read from the keyboard
         Scanner keyboard = new Scanner(System.in);
 
@@ -33,10 +34,10 @@ public class SalesTax {
 
         //display results
         System.out.print(item + "        $");
-        System.out.println(price);
+        System.out.println ( formatter.format(price));
         System.out.print("Tax          $");
-        System.out.println(tax);
+        System.out.println(formatter.format(tax));
         System.out.print("Total        $");
-        System.out.println(total);
+        System.out.println(formatter.format(total));
     }
 }
